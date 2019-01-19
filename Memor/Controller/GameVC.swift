68760aa91dc04b242a9633cc15332d48b901b028
furrki-Mac <admin@Admins-Mac.local>
2019-01-20 00:54:27 +0300

    Bugfix and IBOutlets

--- Memor/Controller/GameVC.swift
+++ Memor/Controller/GameVC.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class GameVC: UIViewController {
 
+    @IBOutlet weak var GameCV: GameCollectionView!
+    
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var roundLabel: UILabel!
+    @IBOutlet weak var timeSlider: GameTimerSlider!
     override func viewDidLoad() {
         super.viewDidLoad()
  
