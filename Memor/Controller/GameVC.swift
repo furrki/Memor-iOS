d2b44b7fe6997a5c1e60dd1d68b59978b7e89e09
furrki-Mac <admin@Admins-Mac.local>
2019-01-20 02:41:46 +0300

    Cell customized

--- Memor/Controller/GameVC.swift
+++ Memor/Controller/GameVC.swift
@@ -44,15 +44,25 @@ class GameVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSo
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "gameCell", for: indexPath)
-        
-        if Game.shared.contents[indexPath.row] {
-            cell.backgroundColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
-        } else {
-            cell.backgroundColor = #colorLiteral(red: 0, green: 0.4784313725, blue: 1, alpha: 1)
+        switch Game.shared.situation {
+        case .show:
+            if Game.shared.contents[indexPath.row] {
+                cell.backgroundColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
+            } else {
+                cell.backgroundColor = #colorLiteral(red: 0, green: 0.4784313725, blue: 1, alpha: 1)
+            }
+            break
+        case .hide:
+            break
+        case .final:
+            break
         }
+       
         
-        // Configure the cell
         return cell
+    }
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        <#code#>
     }
     
     func game(contents: [Bool]) {
@@ -69,5 +79,8 @@ class GameVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSo
             break
             
         }
+    }
+    func initializeLevel(){
+        
     }
 }
--- Memor/View/GameCollectionViewCell.swift
+++ Memor/View/GameCollectionViewCell.swift
@@ -9,5 +9,19 @@
 import UIKit
 
 class GameCollectionViewCell: UICollectionViewCell {
-    
+    var colors: [GameCellType:UIColor] = [
+        .hidden : #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1),
+        .show : #colorLiteral(red: 0.1176470588, green: 0.2156862745, blue: 0.6, alpha: 1),
+        .success : #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1),
+        .error: #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
+    ]
+    var type: GameCellType = .hidden
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        contentView.backgroundColor = colors[type]
+    }
+}
+enum GameCellType {
+    case hidden, show, success, error
 }
