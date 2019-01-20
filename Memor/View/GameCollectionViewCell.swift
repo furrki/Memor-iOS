4beefc853dec3be801d3b66b58d63128b41059a3
furrki-Mac <admin@Admins-Mac.local>
2019-01-20 16:17:29 +0300

    Added Appicon

--- Memor/View/GameCollectionViewCell.swift
+++ Memor/View/GameCollectionViewCell.swift
@@ -16,7 +16,7 @@ class GameCollectionViewCell: UICollectionViewCell {
         .error: #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
     ]
     var type: GameCellType = .hidden {
-        didSet { 
+        didSet {
             contentView.backgroundColor = colors[type]
             contentView.layoutIfNeeded()
         }
