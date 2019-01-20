f50533806ffa54444b388e94dd47c2392132b8f3
furrki-Mac <admin@Admins-Mac.local>
2019-01-20 17:07:18 +0300

    Show Color to orange

--- Memor/View/GameCollectionViewCell.swift
+++ Memor/View/GameCollectionViewCell.swift
@@ -11,7 +11,7 @@ import UIKit
 class GameCollectionViewCell: UICollectionViewCell {
     var colors: [GameCellType:UIColor] = [
         .hidden : #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1),
-        .show : #colorLiteral(red: 0.1176470588, green: 0.2156862745, blue: 0.6, alpha: 1),
+        .show : #colorLiteral(red: 0.8631474743, green: 0.5246582687, blue: 0.006769784112, alpha: 1),
         .success : #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1),
         .error: #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
     ]
